6c4522728c21dbd36c1bac01e08174c407bff8b4
wdj <695685293@qq.com>
2018-06-12 18:32:42 +0800

    代码优化

--- src/main/java/com/myflume/OracleSink.java
+++ src/main/java/com/myflume/OracleSink.java
@@ -8,6 +8,9 @@ import org.apache.flume.*;
 import org.apache.flume.conf.Configurable;
 import org.apache.flume.sink.AbstractSink;
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +21,14 @@ import java.util.Map;
  *
  * @author wdj on 2018/6/8
  */
-public class MySink extends AbstractSink implements Configurable{
+public class OracleSink extends AbstractSink implements Configurable{
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private Integer tryCount = 0;
+
+    //MAX_TRY_COUNT 次尝试提交之后若数据个数还未达到batchSize，则试着提交
+    private final Integer MAX_TRY_COUNT = 2;
 
     private String jdbcurl;
     private String username;
@@ -26,14 +36,23 @@ public class MySink extends AbstractSink implements Configurable{
     private Integer batchSize;
     private String tablename;
     private DataSource dataSource;
+    LogInfoService logInfoService;
+    private List<Map<String,Object>> datas = new ArrayList<>();
+
     // 获取flume的配置参数
     @Override
     public void configure(Context context) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                new String[] { "classpath:spring-context.xml" });
+        applicationContext.start();
+        logInfoService = SpringContextHolder.getBean("logInfoService");
+        dataSource = SpringContextHolder.getBean("dataSource");
         jdbcurl=context.getString("jdbc_url");
         username=context.getString("jdbc_username");
         password=context.getString("jdbc_password");
-        batchSize = context.getInteger("jdbc_batchsize",1000);
-        tablename = context.getString("tablename");
+        batchSize = context.getInteger("jdbc_batchsize",10);
+        tablename = context.getString("jdbc_tablename");
+        logger.info("初始化数据 ==== tablename:"+tablename+";jdbcurl："+jdbcurl+";username:"+username+";batchSize"+batchSize);
     }
 
     // Initialize the connection to the external repository (e.g. HDFS) that
@@ -57,23 +76,27 @@ public class MySink extends AbstractSink implements Configurable{
     // additional cleanup
     @Override
     public synchronized void stop() {
+        logger.info("sink关闭。。。。。。。。保存缓存中的剩余数据");
+        if(datas != null && !datas.isEmpty()){
+            logInfoService.save(tablename,datas);
+            logger.info("提交"+datas.size()+"条数据");
+        }
         dataSource.close();
         super.stop();
     }
 
-    // log日志数据格式需要相同，具体格式以第一个为准
     @Override
     public Status process() throws EventDeliveryException {
-        LogInfoService logInfoService = SpringContextHolder.getBean("logInfoService");
         Status status = null;
         // Start transaction
-
         Channel ch = getChannel();
         Transaction txn = ch.getTransaction();
         txn.begin();
         try {
+            if(StringUtils.isBlank(tablename)){
+                throw new Exception("tablename不能为空！");
+            }
             // This try clause includes whatever Channel operations you want to do
-            List<Map<String,Object>> datas = new ArrayList<>();
             long processedEvent = 0;
             for (; processedEvent < batchSize; processedEvent++) {
                 Event event = ch.take();
@@ -91,28 +114,51 @@ public class MySink extends AbstractSink implements Configurable{
                         }
                     }
                 }else{
+                    logger.info("even为空，回退。。。");
                     status = Status.BACKOFF;
                     break;
                 }
             }
-            if(!StringUtils.isBlank(tablename) && datas!=null && !datas.isEmpty()){
-                logInfoService.save(tablename,datas);
+            boolean canCommit = (status != Status.BACKOFF && datas!=null && !datas.isEmpty())
+                    || (tryCount >= MAX_TRY_COUNT && datas!=null && !datas.isEmpty());
+            // 将数据复制到临时变量，将data去空，当时若flume在datas浮空后未保存数据就关闭，则还是会丢失一部分数据
+            List<Map<String,Object>> tem = new ArrayList<>();
+            tem.addAll(datas);
+            datas = new ArrayList<>();
+            if(canCommit){
+                logInfoService.save(tablename,tem);
+                logger.info("提交"+datas.size()+"条数据");
                 status = Status.READY;
+                tryCount=0;
+                txn.commit();
+            }else if(status == Status.BACKOFF){
+                txn.rollback();
+                tryCount++;
+            }else{
+                logger.info("数据为空！");
+                status = Status.BACKOFF;
+                txn.rollback();
+                tryCount=0;
             }
-            txn.commit();
-        } catch (Throwable t) {
-
+        } catch (Exception e) {
             txn.rollback();
             // Log exception, handle individual exceptions as needed
-
+            logger.error("保存数据出错：",e);
             status = Status.BACKOFF;
-            // re-throw all Errors
-            if (t instanceof Error) {
-                System.out.println("[ERROR][process]"+ t.toString());
-                throw (Error)t;
-            }
         }
+        txn.close();
         return status;
     }
 
+    public static void main(String[] args){
+        OracleSink oracleSink = new OracleSink();
+        oracleSink.configure(null);
+        oracleSink.start();
+        try {
+            oracleSink.process();
+        } catch (EventDeliveryException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
